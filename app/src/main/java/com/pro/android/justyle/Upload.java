34f629ace7a31c5f9a6dfa1348972fa3f1e0ea41
EmperorOfTrees <EmperorOfTrees@users.noreply.github.com>
2019-05-13 13:33:54 +0200

    You can see the email of the uploader on items in the wardrobe and marketplace.

---
+++ app/src/main/java/com/pro/android/justyle/Upload.java
@@ -0,0 +1,78 @@
+package com.pro.android.justyle;
+
+import com.google.firebase.database.Exclude;
+
+public class Upload{
+private String mName;
+private String mImageUrl;
+private String mDescription;
+private String mKey;
+private String mUserName;
+
+
+
+        /**
+         * empty constructor needed
+         */
+public Upload(){
+
+        }
+
+
+Upload(String name, String imageUrl, String description, String userName) {
+
+        if (name.trim().equals("")){
+        name = "No name";
+        }
+
+        if (description.trim().equals("")){
+                description = "No description";
+        }
+
+        mName = name;
+        mImageUrl = imageUrl;
+        mDescription = description;
+        mUserName = userName;
+
+
+        }
+
+        public String getUserName() {
+                return mUserName;
+        }
+
+        public void setUserName(String userName) {
+                mUserName = userName;
+        }
+
+        public String getName(){
+        return mName;
+        }
+
+public void setName (String name){
+        mName = name;
+        }
+
+String getImageUrl(){
+        return mImageUrl;
+        }
+
+public void setImageUrl (String imageUrl){
+        mImageUrl = imageUrl;
+        }
+
+        String getDescription(){
+                return mDescription;
+        }
+
+        public void setDescription (String description){
+                mDescription = description;
+        }
+@Exclude // we don't want this in firebase database because we already have a key
+String getKey(){
+        return mKey;
+}
+        @Exclude
+        void setKey(String key){ mKey = key; }
+
+}
